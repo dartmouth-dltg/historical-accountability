079f412886420787c5b917bf28d14a356f856eb4
Bill Kennedy <bill@agilehumanities.ca>
2020-06-10 18:01:20 -0400

    Item Page Changes + Universal Viewer
    - Modified item page width
    - Added slideshow to default template (in case there are multiple media items)
    - Media is replaced by Universal Viewer (via JS) if UV exists
    - Universal Viewer recolourized to match theme
    - Universal frame modified to match theme
    
    Signed-off-by: Bill Kennedy <bill@agilehumanities.ca>

---
+++ source/js/universalViewer.js
@@ -0,0 +1,15 @@
+/**
+ *  @file universalViewer.js
+ *
+ *  Replaces standard item media view with Universal Viewer if it exists
+ *
+ */
+
+jQuery(document).ready(function() {
+  
+  if ($('.universal-viewer').length > 0 && $('.media-list').length > 0) {
+    var wrapper = $('.media-list').parent();
+    $('.media-list').remove();
+    $('.universal-viewer').prependTo(wrapper);
+  }
+});
