4f1c2baa304cc876bb35001098b07b2d847dce2a
unknown <nando.rambiriche@gmail.com>
2020-06-10 15:09:58 -0400

    fixed sub nav position without admin bar, added another level to sub nav

--- source/js/navSubMenu.js
+++ source/js/navSubMenu.js
@@ -6,14 +6,43 @@ $(document).ready(function(){
         $(this).children().show();
     });
 
+    $('nav.desktop-nav > .navigation > li > ul > li').mouseover( function() {
+        $("nav.desktop-nav > .navigation > li > ul > li > ul").hide();
+        $(this).children().show();
+    });
+
+    $('nav.desktop-nav > .navigation > li > ul > li > a').mouseover( function() {
+        $('nav.desktop-nav > .navigation > li > ul > li > a').css("color", "#1d2c27");
+        $(this).css("color", "grey");
+    });
+
     $('#primary-content').mouseover(function() {
         $("nav.desktop-nav > .navigation > li > ul").hide();
     });
+    
+    $('#splash').mouseover(function() {
+        $("nav.desktop-nav > .navigation > li > ul").hide();
+    });
+
+
+
+
 
     $(window).resize(function() {
-        $(".navigation > li > ul").css("top", $("header").height() + 10);
+        if ( $('#user-bar').length !== 0 ) {
+            $(".navigation > li > ul").css("top", $("header").height() + 43);
+        }
+        else {
+            $(".navigation > li > ul").css("top", $("header").height() + 5);
+        }
+
     });
-    
+    if ( $('#user-bar').length !== 0 ) {
+        $(".navigation > li > ul").css("top", $("header").height() + 43);
+    }
+    else {
+        $(".navigation > li > ul").css("top", $("header").height() + 5);
+    }
     $(window).trigger('resize');
 
 });
