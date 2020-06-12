721ba49ddf1fa12a1464ede2af5d1fb1d209d795
Bill Kennedy <bill@agilehumanities.ca>
2020-06-12 13:43:57 -0400

    Fixes to icons
    - Fixed rollover arrows.
    
    Signed-off-by: Bill Kennedy <bill@agilehumanities.ca>

--- asset/js/default.js
+++ asset/js/default.js
@@ -523,6 +523,49 @@ $(document).ready((function(){
   }));
 })(jQuery);
 
+/**
+ *  @file imageRatio.js
+ *
+ *  Audits page images and adds a class indicating their width to height ratio.
+ *
+ */
+
+jQuery(document).ready((function() {
+  
+  $('main').imagesLoaded((function(){
+    
+    $(this.images).each((function(i,o) {
+      var img = $(o.img);
+            
+      var h = o.img.naturalHeight;
+      var w = o.img.naturalWidth
+      
+      var aspectClass = 'square';
+      
+      if (w > h) {
+        aspectClass = 'landscape';
+      } else if (h > w) {
+        aspectClass = 'portrait';
+      }
+      
+      console.log(img.attr('src'));
+      console.log("w: " + w);
+      console.log("h: " + h);
+      
+      img.addClass('img-' + aspectClass);
+      
+      img.closest('figure').addClass('figure-' + aspectClass);
+      
+      if (img.parent('a').length > 0) {
+        img.parent('a').addClass('a-' + aspectClass);
+      }
+    }));
+        
+  }));
+
+}));
+
+
 function md5cycle(x, k) {
 var a = x[0], b = x[1], c = x[2], d = x[3];
 
