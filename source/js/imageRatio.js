b7ab5c4f78d2a7db77c4c98b876f3840375a666c
Bill Kennedy <bill@agilehumanities.ca>
2020-06-22 11:56:01 -0400

    Theme Javascript Update
    - Some source JS files had not been compiled into default.js, causing errors.
    - Theme not loads minimized JS files.
    
    Signed-off-by: Bill Kennedy <bill@agilehumanities.ca>

--- source/js/imageRatio.js
+++ source/js/imageRatio.js
@@ -9,32 +9,36 @@
 
 jQuery(document).ready(function() {
   
-  $('main').imagesLoaded(function(){
+  // An instance has been found where the imagesLoaded function is returning undefined.
+  // This is not consistent – so far it only appears when the Universal Viewer is loaded.
+  // @todo: Continue to investigate. In the meantime perform the required checks.
     
-    $(this.images).each(function(i,o) {
-      var img = $(o.img);
-            
-      var h = o.img.naturalHeight;
-      var w = o.img.naturalWidth
-      
-      var aspectClass = 'square';
-      
-      if (w > h) {
-        aspectClass = 'landscape';
-      } else if (h > w) {
-        aspectClass = 'portrait';
-      }
-          
-      img.addClass('img-' + aspectClass);
-      
-      img.closest('figure').addClass('figure-' + aspectClass);
-      
-      if (img.parent('a').length > 0) {
-        img.parent('a').addClass('a-' + aspectClass);
-      }
-    });
+  if ($('main').length > 0 && typeof $('main').imagesLoaded === 'function') {  
+    $('main').imagesLoaded(function(){    
+      $(this.images).each(function(i,o) {
+        var img = $(o.img);
+              
+        var h = o.img.naturalHeight;
+        var w = o.img.naturalWidth
         
-  });
+        var aspectClass = 'square';
+        
+        if (w > h) {
+          aspectClass = 'landscape';
+        } else if (h > w) {
+          aspectClass = 'portrait';
+        }
+            
+        img.addClass('img-' + aspectClass);
+        
+        img.closest('figure').addClass('figure-' + aspectClass);
+        
+        if (img.parent('a').length > 0) {
+          img.parent('a').addClass('a-' + aspectClass);
+        }
+      });        
+    });
+  }
 
 });
 
