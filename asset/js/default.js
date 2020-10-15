6727c9692693668cf37256df40c7d7d4f035d956
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2020-10-15 11:25:27 -0400

    update svgs; remove dark mode

--- asset/js/default.js
+++ asset/js/default.js
@@ -159,36 +159,36 @@ jQuery(document).ready((function() {
  * 
  */
  
- $(document).ready((function(){
-
-    function darkOn(){
-        $("body").addClass('dark');
-        $('.dark-switch > input[type="checkbox"]').prop("checked", true); //sync mobile and desktop btns
-    }
-
-    function darkOff(){
-        $('.dark-switch > input[type="checkbox"]').prop("checked", false); //sync mobile and desktop btns
-        $("body").removeClass('dark');
-    }
-    
-    if(localStorage.getItem("darkmode1") === "on"){
-        darkOn();
-    }
-    else if(localStorage.getItem("darkmode1") === "off"){
-        darkOff();
-    }
-
-    $('.dark-switch > input[type="checkbox"]').click((function(){ //dark mode checkbox check
-        if($(this).prop("checked") == true){
-            localStorage.setItem("darkmode1", "on");
-            darkOn();
-        }
-        else if($(this).prop("checked") == false){
-            localStorage.setItem("darkmode1", "off");
-            darkOff();
-        }
-    }));
-}));
+// $(document).ready(function(){
+//
+//    function darkOn(){
+//        $("body").addClass('dark');
+//        $('.dark-switch > input[type="checkbox"]').prop("checked", true); //sync mobile and desktop btns
+//    }
+//
+//    function darkOff(){
+//        $('.dark-switch > input[type="checkbox"]').prop("checked", false); //sync mobile and desktop btns
+//        $("body").removeClass('dark');
+//    }
+//    
+//    if(localStorage.getItem("darkmode1") === "on"){
+//        darkOn();
+//    }
+//    else if(localStorage.getItem("darkmode1") === "off"){
+//        darkOff();
+//    }
+//
+//    $('.dark-switch > input[type="checkbox"]').click(function(){ //dark mode checkbox check
+//        if($(this).prop("checked") == true){
+//            localStorage.setItem("darkmode1", "on");
+//            darkOn();
+//        }
+//        else if($(this).prop("checked") == false){
+//            localStorage.setItem("darkmode1", "off");
+//            darkOff();
+//        }
+//    });
+//});
 /*!
  * jQuery throttle / debounce - v1.1 - 3/7/2010
  * http://benalman.com/projects/jquery-throttle-debounce-plugin/
